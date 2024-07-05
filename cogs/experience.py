23b34846dcefcfc541ced99c7ded42f0f9746b21
AltarCrossorm <eng.cresp@gmail.com>
2024-07-05 15:42:38 +0200

    Added a level up system, and also commented the experience class

--- cogs/experience.py
+++ cogs/experience.py
@@ -1,5 +1,6 @@
 import nextcord
 from nextcord.ext import commands
+from math import exp
 import sqlite3
 import datetime
 from config import DATABASE_DIR
@@ -7,12 +8,16 @@ from config import DATABASE_DIR
 class experience(commands.Cog):
 
     def __init__(self, bot):
+        """
+        Initialize experience class
+        :param bot: The bot Discord instancy.
+        """
+
         self.bot = bot
         self.con = sqlite3.connect(DATABASE_DIR)
         self.cur = self.con.cursor()
-        self.res = []
-        self.com = []
-        try:
+
+        try: # Checking if the database is opennable
             self.con.cursor()
             return print("Database ready")
         except Exception as ex:
@@ -20,69 +25,105 @@ class experience(commands.Cog):
     
     @commands.Cog.listener()
     async def on_message(self,message):
+        """
+        Event invoked whenever a message can be seen by the bot
+        :param message: The message sent.
+        """
 
-        if message.author.bot:
+        if message.author.bot: # Checks if the sender isn't a bot 
             return
 
+        # Declaring variables who will be used later
         user_id = message.author.id
         message_length = len(message.content)
+
+        # Checks if the used exists in the Database
         query_ck = f"""
         SELECT id, xp 
         FROM levels 
         WHERE id = {user_id};
         """
-        
         self.cur.execute(query_ck)
-        user = self.cur.fetchone()
 
+        # Checks if user is empty or not
+        user = self.cur.fetchone()
         if user:
+
+            # Grants more xp by the size of the text
             query_update = f"""
             UPDATE levels
-            SET xp = xp + (10 + 0.01 * {message_length})
+            SET xp = xp + (5 + 0.02 * {message_length}), xp_total = xp_total + (5 + 0.02 * {message_length})
             WHERE id = {user_id};
             """
             self.cur.execute(query_update)
-            if(self.cur.rowcount > 0):
-                print(f"Niveau augmente ! utilisateur :{message.author}, valeur : {10 + 0.01 * message_length}")
 
-                with open("./logs/logs_levels_DB.log", "a") as fichier:
-                    fichier.write(f"{datetime.datetime.now()} | INFO | user : @{message.author} | xp gained : {10 + 0.01 * message_length} | channel : {message.channel.id} ({message.channel.name})\n")
-                    print("Log envoyé")
-            else:
-                print("Error databaase, please check it up")
+            # Write the xp gain into the .log file
+            with open("./logs/logs_levels_DB.log", "a") as fichier:
+                fichier.write(f"{datetime.datetime.now()} | INFO | user : @{message.author} | xp gained : {10 + 0.01 * message_length} | channel : {message.channel.id} ({message.channel.name})\n")
 
         else:
+            # Create a new section for the user
             query_insert = f"""
-            INSERT INTO levels(id,xp)
-            VALUES({user_id}, 10);
+            INSERT INTO levels(id, xp, level, xp_total)
+            VALUES({user_id}, 5, 0, 5);
             """
-            self.com = self.cur.execute(query_insert)
+            self.cur.execute(query_insert)
+
+            # Write the user insert into the .log file
             with open("logs\logs_levels_DB.log", "a") as fichier:
                 fichier.write(f"{datetime.datetime.now()} | INFO | new user registered : @{message.author}\n")
-                print("New user added to the database")
+                
 
+        # Get xp and level infos
+        xp_db = self.cur.execute(f"SELECT xp FROM levels WHERE id = {user_id}").fetchone()
+        level_db = self.cur.execute(f"SELECT level FROM levels WHERE id = {user_id}").fetchone()
+
+        # Create variables
+        xp = xp_db[0]
+        level = level_db[0]
+        exp_level = 10 * exp(0.2*level)
+
+        # Check if xp is enough for an level up
+        if(xp > exp_level):
+
+            # Retrive xp from the user, but not the total xp
+            self.cur.execute(f"UPDATE levels SET xp = xp - {exp_level}, level = level +1 WHERE id = {user_id}")
+
+            # Write the user insert into the .log file
+            with open("logs\logs_levels_DB.log", "a") as fichier:
+                fichier.write(f"{datetime.datetime.now()} | INFO | @{message.author} got to level {level+1}\n")
+            await message.channel.send(f"Félicitations {message.author.mention}, vous avez atteint le niveau {level + 1} !")
+        
+        # Commit all changes
         self.con.commit()
+
+
+
+    @commands.command(help = "Display the 5 most experienced users")
+    async def topRank(self,ctx):
         
-    @commands.command(help = "draws the 5 most experienced users")
-    async def dataTest(self,ctx):
-
-        xp = []
-
-        for row in self.cur.execute("SELECT * FROM levels;").fetchmany(5):
-            user_id = row[0]
-            user_xp = row[1]
-            user = self.bot.get_user(user_id) or await self.bot.fetch_user(user_id)
-            xp.append((user,user_xp))
         
-        description = '\n'.join([f'{user.name}: {xp} XP' for user, xp in xp])
+        xp = [] # Create list for fetching data
 
-        mbed = nextcord.Embed(
+        # Fetching the first 5 users into the xp variable
+        for row in self.cur.execute("SELECT * FROM levels;").fetchmany(5): 
+            user_id = row[0] # Getting user id
+            user_lv = row[2] # Getting user level
+            user_xp = row[3] # Getting user total xp
+            
+            xp.append((user_id,user_lv,user_xp)) # Insert data in the list
+        
+        # make data ready to be displayed
+        description = '\n'.join([f'<@{user_id}>: Level {user_lv} ({user_xp} XP)' for user_id, user_lv, user_xp in xp])
+
+        # Create the embed to display de xp top
+        e = nextcord.Embed(
             title="Most 5 experienced users",
             description = description,
             colour=(nextcord.Colour.green())
         )
 
-        await ctx.send(embed=mbed)
+        await ctx.send(embed=e) # Send th message
     
 
 async def setup(bot):
